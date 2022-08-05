9302b14baf5f135d6bd262b044250b4d24fecd44
mhvozdesky <m.hvozdesky@gmail.com>
2022-08-05 13:41:36 +0300

    changed weather_formatter. Added Weather in , added padding

--- weather_formatter.py
+++ weather_formatter.py
@@ -1,8 +1,11 @@
+import os
 from datetime import datetime
 
 
 def format_weather(weather: dict) -> str:
     weather_text = ''
+    weather_text += f'Weather in {os.environ.get("CITY")}\n'
+
     weather_list = weather['list']
 
     for weather_item in weather_list:
@@ -11,20 +14,20 @@ def format_weather(weather: dict) -> str:
         weather_item_main = weather_item['main']
 
         weather_text += date + '\n'
-        weather_text += f'temp: {weather_item_main["temp"]}, feels_like: ' \
+        weather_text += f'  temp: {weather_item_main["temp"]}, feels_like: ' \
                         f'{weather_item_main["feels_like"]}, temp_min: ' \
                         f'{weather_item_main["temp_min"]}, temp_max: ' \
                         f'{weather_item_main["temp_max"]}' + '\n'
 
-        weather_text += 'weather: '
+        weather_text += '  weather: '
         for index, i in enumerate(weather_item['weather']):
             sep = '' if index == 0 else ', '
             weather_text += sep + i['description']
 
         weather_text += '\n'
-        weather_text += f'clouds: {weather_item["clouds"]["all"]}' + '\n'
+        weather_text += f'  clouds: {weather_item["clouds"]["all"]}' + '\n'
 
-        weather_text += f'wind: {weather_item["wind"]["speed"]}'
+        weather_text += f'  wind: {weather_item["wind"]["speed"]}'
 
         weather_text += '\n\n'
 
